97b41c6280d0c2c500a7dfbe2c23231fe0d5e711
CeliaTHP <Tibiza121>
2022-04-01 15:45:41 +0200

    - Add dropdown menu when selecting type, use Material EditText for inputs,

--- app/src/main/java/com/example/oc_p9_kotlin/activities/AddEstateActivity.kt
+++ app/src/main/java/com/example/oc_p9_kotlin/activities/AddEstateActivity.kt
@@ -7,6 +7,7 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.ListPopupWindow
 import androidx.core.content.res.ResourcesCompat
 import com.example.oc_p9_kotlin.R
 import com.example.oc_p9_kotlin.databinding.ActivityAddEstateBinding
@@ -50,9 +51,40 @@ class AddEstateActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
             Log.d(TAG, "can Create Estate")
         }
+
+        binding.addEstateTypeButton.setOnClickListener {
+            onEstateTypeButtonClick()
+        }
+
+
+    }
+
+    private fun onEstateTypeButtonClick() {
+
+
+        val listPopupWindow =
+            ListPopupWindow(this, null, com.google.android.material.R.attr.listPopupWindowStyle)
+        listPopupWindow.anchorView = binding.addEstateTypeButton
+        listPopupWindow.setAdapter(
+            ArrayAdapter(
+                this,
+                R.layout.dropdown_menu_popup_item,
+                arrayOf("type1", "type2", "type3")
+            )
+        )
+
+        listPopupWindow.setOnItemClickListener { _, _, position: Int, _ ->
+            //callback(position)
+            //setType
+            listPopupWindow.dismiss()
+        }
+        listPopupWindow.show()
+
+
     }
 
     private fun initSpinner() {
+
 
         ArrayAdapter.createFromResource(
             this,
@@ -62,9 +94,9 @@ class AddEstateActivity : AppCompatActivity() {
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             // Apply the adapter to the spinner
-            binding.addEstateTypeSpinner.adapter = adapter
+            // binding.addEstateTypeSpinner.adapter = adapter
         }
-
+/*
         binding.addEstateTypeSpinner.onItemSelectedListener =
             object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, id: Long) {
@@ -76,6 +108,8 @@ class AddEstateActivity : AppCompatActivity() {
                 }
 
             }
+
+ */
     }
 
 }
